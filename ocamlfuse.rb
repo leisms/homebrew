28845dc2febf21f6ce2cd12c49bbea119c919162
Le Zhang <le@squadle.com>
2018-01-09 10:29:10 -0500

    Update ocamlfuse.rb

--- ocamlfuse.rb
+++ ocamlfuse.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class ocamlfuse < Formula
+class Ocamlfuse < Formula
   url "https://github.com/astrada/google-drive-ocamlfuse/tarball/master.tar.gz"
   homepage "https://github.com/astrada/google-drive-ocamlfuse"
   sha1 "dc5c0563c2a43072c53d23fd7e51aafe0b79474c"
